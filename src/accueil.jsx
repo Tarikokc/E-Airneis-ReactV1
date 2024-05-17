9b48cd655b264436671cef32794b1d19019ea307
Haile <hailelsavadoux@gmail.com>
2024-05-17 15:12:23 +0200

    je remets comme avant

--- src/accueil.jsx
+++ src/accueil.jsx
@@ -5,7 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 
 const productCategories = [
   { id: 1, title: 'CATÉGORIE 1', description: 'Description de la catégorie 1', products: [{ image: '/images/product1.jpg', title: 'PROD 1', description: 'Description du produit 1' }, { image: '/images/product2.jpg', title: 'PROD 2', description: 'Description du produit 2' }, { image: '/images/product3.jpg', title: 'PROD 3', description: 'Description du produit 3' }] },
-  { id: 2, title: 'CATÉGORIE 2', description: 'Description de dssdala catégorie 2', products: [{ image: '/images/product4.jpg', title: 'PROD 4', description: 'Description du produit 4' }, { image: '/images/product5.jpg', title: 'PROD 5', description: 'Description du produit 5' }, { image: '/images/product6.jpg', title: 'PROD 6', description: 'Description du produit 6' }] },
+  { id: 2, title: 'CATÉGORIE 2', description: 'Description de la catégorie 2', products: [{ image: '/images/product4.jpg', title: 'PROD 4', description: 'Description du produit 4' }, { image: '/images/product5.jpg', title: 'PROD 5', description: 'Description du produit 5' }, { image: '/images/product6.jpg', title: 'PROD 6', description: 'Description du produit 6' }] },
   { id: 3, title: 'CATÉGORIE 3', description: 'Description de la catégorie 3', products: [{ image: '/images/product7.jpg', title: 'PROD 7', description: 'Description du produit 7' }, { image: '/images/product8.jpg', title: 'PROD 8', description: 'Description du produit 8' }, { image: '/images/product9.jpg', title: 'PROD 9', description: 'Description du produit 9' }] },
 ]; 
 
