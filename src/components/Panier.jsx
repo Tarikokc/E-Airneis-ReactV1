a5d92e77dea31341df1b51d2da5cb45c9233ecbd
Tarikokc <tarik.oukaci@gmail.com>
2024-07-27 17:39:41 +0200

    Evolution du panier/Caroussel ProductSingle

--- src/components/Panier.jsx
+++ src/components/Panier.jsx
@@ -2,138 +2,162 @@ import React, { useState, useEffect } from 'react';
 import '../css/Panier.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom'; // Importer useNavigate
+import { useNavigate, useLocation } from 'react-router-dom'; // Importer useNavigate
 import { ImBin2 } from "react-icons/im";
 
 const baseUrl = '/img/';
 
 function Panier() {
-  const [panier, setPanier] = useState([]); 
+  const [panier, setPanier] = useState([]);
   const [userId, setUserId] = useState(null);
   const [productId, setProductId] = useState(null);
 
   const navigate = useNavigate(); // Obtenir l'objet history
   const [totalTTC, setTotalTTC] = useState(0);
+  const location = useLocation();
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem('user'));
     if (userData && userData.user && userData.user.id) {
       setUserId(userData.user.id);
+      fetchPanier(userData.user.id);
     } else {
-      console.error('User data or token is missing or invalid:', userData);
+      const guestCart = JSON.parse(localStorage.getItem('guestCart')) || [];
+      setPanier(guestCart);
+      fetchPanier(); 
+
     }
   }, []);
 
-  useEffect(() => {
-    if (userId) {
-      const fetchPanier = async () => {
-        try {
-          const response = await fetch(`http://localhost:8000/api/panier/${userId}`);
-          const data = await response.json();
-          console.log("Données du panier :", data); // Afficher les données du panier dans la console
+  const fetchPanier = async (userId = null) => {
+    try {
+      if (userId) {
+        const response = await fetch(`http://localhost:8000/api/panier/${userId}`);
+        const data = await response.json();
+        setPanier(data);
+      } else {
+        const guestCart = JSON.parse(localStorage.getItem('guestCart')) || [];
 
-          setPanier(data);
-        } catch (error) {
-          console.error('Erreur lors de la récupération du panier:', error);
-        }
-      };
+        const formattedGuestCart = guestCart.map(item => ({
+          productId: parseInt(item.productId, 10) || 0,
+          Nom: item.Nom || '',
+          prix: parseFloat(item.prix) || 0,
+          quantite: parseInt(item.quantite, 10) || 0,
+          photoUrl: item.photoUrl || ''
+        }));
 
-      fetchPanier();
+        setPanier(formattedGuestCart);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération du panier:', error);
     }
-  }, [userId]);
+  };
 
   const handleModifierProduit = async (productId, nouvelleQuantite) => {
-    try {
-      const userData = JSON.parse(localStorage.getItem('user'));
-      const userId = userData.user.id;
+    console.log("Début de handleModifierProduit");
+    console.log("productId:", productId, "nouvelleQuantite:", nouvelleQuantite);
 
-      console.log("Envoi de la requête PUT avec les données suivantes :");
-      console.log("productId:", productId);
-      console.log("userId:", userId);
-      console.log("nouvelleQuantite:", nouvelleQuantite);
+    const userData = JSON.parse(localStorage.getItem('user'));
 
-      const response = await fetch(`http://localhost:8000/api/panier/${productId}/${userId}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ quantite: nouvelleQuantite }),
-      });
+    if (userData && userData.user && userData.user.id) {
+      console.log("Utilisateur connecté, modification via API");
+      try {
+        const userId = userData.user.id;
+        console.log("userId:", userId);
 
-      console.log("Réponse de l'API :", response);
+        const response = await fetch(`http://localhost:8000/api/panier/${productId}/${userId}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ quantite: nouvelleQuantite }),
+        });
 
-      if (response.ok) {
-        const updatedPanierData = await response.json();
-        console.log("Données du panier mis à jour :", updatedPanierData);
+        console.log("Réponse API (PUT):", response);
 
-        const updatedPanierItem = updatedPanierData.panier;
-        setPanier(prevPanier => prevPanier.map(p =>
-          p.productId === productId ? { ...p, quantite: updatedPanierItem.quantite } : p
-        ));
-        toast.success('Quantité mise à jour !');
-      } else {
-        console.error('Erreur lors de la modification:', response.statusText);
-        toast.error('Erreur lors de la modification de la quantité.');
+        if (response.ok) {
+          const updatedPanierData = await response.json();
+          console.log("Données du panier mis à jour (API):", updatedPanierData);
 
-        // Récupérer le message d'erreur détaillé depuis la réponse
-        try {
-          const errorData = await response.json();
-          console.error("Détails de l'erreur :", errorData);
-          if (errorData.error) {
-            toast.error(errorData.error);
+          const updatedPanierItem = updatedPanierData.panier;
+          setPanier(prevPanier => prevPanier.map(p =>
+            p.productId === productId ? { ...p, quantite: updatedPanierItem.quantite } : p
+          ));
+          toast.success('Quantité mise à jour !');
+        } else {
+          console.error('Erreur API (PUT):', response.statusText);
+          toast.error('Erreur lors de la modification de la quantité.');
+
+          try {
+            const errorData = await response.json();
+            console.error("Détails de l'erreur API (PUT):", errorData);
+            if (errorData.error) {
+              toast.error(errorData.error);
+            }
+          } catch (parseError) {
+            console.error("Erreur lors de l'analyse de la réponse d'erreur (PUT):", parseError);
           }
-        } catch (parseError) {
-          console.error("Erreur lors de l'analyse de la réponse d'erreur :", parseError);
         }
+      } catch (error) {
+        console.error('Erreur requête (PUT):', error);
+        toast.error('Une erreur est survenue.');
       }
-    } catch (error) {
-      console.error('Erreur lors de la requête:', error);
-      toast.error('Une erreur est survenue.');
+    } else {
+      console.log("Utilisateur non connecté, modification localStorage");
+
+      const updatedPanier = panier.map(p =>
+        p.productId === productId ? { ...p, quantite: nouvelleQuantite } : p
+        
+      );
+      localStorage.setItem('guestCart', JSON.stringify(updatedPanier));
+      toast.success('Quantité mise à jour avec succès !'); // Toast de succès
+
+
+      setPanier(updatedPanier);
+      console.log("Panier après modification (localStorage):", updatedPanier); 
     }
   };
 
-  // const handleSupprimerProduit = async (productId) => { // Utilisez panierId
-  //   try {
-  //     const userData = JSON.parse(localStorage.getItem('user'));
-  //     const userId = userData.user.id;
+  const handleSupprimerProduit = async (productId) => {
+    console.log("Début de handleSupprimerProduit");
+    console.log("productId:", productId);
 
-  //     const response = await fetch(`http://localhost:8000/api/panier/${productId}/${userId}`, {
-  //       method: 'DELETE',
-  //     });
+    const userData = JSON.parse(localStorage.getItem('user'));
 
-  //     if (response.ok) {
-  //       setPanier(prevPanier => prevPanier.filter(p => p.panierId !== panierId));
-  //       toast.success('Produit supprimé du panier !');
-  //     } else {
-  //       console.error('Erreur lors de la suppression:', response.statusText);
-  //       toast.error('Erreur lors de la suppression du produit.');
-  //     }
-  //   } catch (error) {
-  //     console.error('Erreur lors de la requête:', error);
-  //     toast.error('Une erreur est survenue.');
-  //   }
-  // };
+    if (userData && userData.user && userData.user.id) {
+      console.log("Utilisateur connecté, suppression via API");
+      try {
+        const userId = userData.user.id;
+        console.log("userId:", userId);
 
-  const handleSupprimerProduit = async (productId) => { 
-    try {
-      const userData = JSON.parse(localStorage.getItem('user'));
-      const userId = userData.user.id;
+        const response = await fetch(`http://localhost:8000/api/panier/${productId}/${userId}`, {
+          method: 'DELETE',
+        });
 
-      const response = await fetch(`http://localhost:8000/api/panier/${productId}/${userId}`, {
-        method: 'DELETE',
-      });
+        console.log("Réponse API (DELETE):", response);
 
-      if (response.ok) {
-        // Filtrer le panier en fonction du productId
-        setPanier(prevPanier => prevPanier.filter(p => p.productId !== productId)); 
-        toast.success('Produit supprimé du panier !');
-      } else {
-        console.error('Erreur lors de la suppression:', response.statusText);
-        toast.error('Erreur lors de la suppression du produit.');
+        if (response.ok) {
+          setPanier(prevPanier => prevPanier.filter(p => p.productId !== productId));
+          toast.success('Produit supprimé du panier !');
+        } else {
+          console.error('Erreur API (DELETE):', response.statusText);
+          toast.error('Erreur lors de la suppression du produit.');
+        }
+      } catch (error) {
+        console.error('Erreur requête (DELETE):', error);
+        toast.error('Une erreur est survenue.');
       }
-    } catch (error) {
-      console.error('Erreur lors de la requête:', error);
-      toast.error('Une erreur est survenue.');
+    } else {
+      console.log("Utilisateur non connecté, suppression localStorage");
+
+      const updatedPanier = panier.filter(p => p.productId !== productId);
+      localStorage.setItem('guestCart', JSON.stringify(updatedPanier));
+      toast.success('Produit supprimé du panier avec succès !'); // Toast de succès
+
+
+      setPanier(updatedPanier);
+      console.log("Panier après suppression (localStorage):", updatedPanier); 
     }
   };
+
   const calculerTotal = () => {
     const totalHT = panier.reduce((total, produit) => total + produit.prix * produit.quantite, 0);
     const tauxTVA = 0.2;
@@ -150,7 +174,7 @@ function Panier() {
   useEffect(() => {
     // Mettez à jour le total TTC lorsque le panier change
     setTotalTTC(calculerTotal().totalTTC);
-  }, [panier]); // Déclenchez l'effet à chaque changement du panier
+  }, [panier]); 
 
   const handlePasserCommande = () => {
     navigate('/StripeForm');
@@ -162,8 +186,7 @@ function Panier() {
       <ul className="liste-produits">
         {panier.map((produit) => (
           <li key={produit.productId} className="produit-panier">
-            {/* Affichage de l'image */}
-            {produit.photoUrl && ( // Vérifier si photoUrl existe
+            {produit.photoUrl && ( 
               <img src={baseUrl + produit.photoUrl} alt={produit.Nom} />
             )}
             <div className="details-produit">
@@ -182,7 +205,7 @@ function Panier() {
                 }}>+</button>
               </div>
               <button onClick={() => handleSupprimerProduit(produit.productId)} className="supprimer-produit">
-                <ImBin2 /> 
+                <ImBin2 />
               </button>
             </div>
           </li>
@@ -193,10 +216,10 @@ function Panier() {
         <p>dont TVA : {calculerTotal().montantTVA.toFixed(2)} €</p>
       </div>
       <button className="passer-commande" onClick={() => {
-        console.log("Total TTC transmis à Checkout:", totalTTC); // Log avant la navigation
+        console.log("Total TTC transmis à Checkout:", totalTTC); 
         sessionStorage.setItem('totalTTC', totalTTC);
         navigate('/checkout');
-        }}>
+      }}>
         Passer la commande
       </button>
       <ToastContainer />
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { VscSearch, VscThreeBars } from 'react-icons/vsc';
 import { ShoppingCart } from 'lucide-react';
 import '../css/Nav.css';
@@ -7,6 +7,8 @@ import Menu from './menu';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [panierCount, setPanierCount] = useState(0);
+
   const navigate = useNavigate();
 
   const toggleMenu = () => {
@@ -21,6 +23,33 @@ function Navbar() {
     navigate('/Panier'); // Rediriger vers la page Panier
   };
 
+  useEffect(() => {
+    const fetchPanierCount = async () => {
+      const userData = JSON.parse(localStorage.getItem('user'));
+      if (userData && userData.user && userData.user.id) {
+        // Utilisateur connecté : récupérer le nombre depuis le backend (route existante)
+        try {
+          const response = await fetch(`http://localhost:8000/api/panier/${userData.user.id}`);
+          if (response.ok) {
+            const data = await response.json();
+            const count = data.length; 
+            setPanierCount(count);
+          } else {
+            console.error("Erreur lors de la récupération du panier:", response.statusText);
+          }
+        } catch (error) {
+          console.error('Erreur lors de la récupération du nombre d\'articles :', error);
+        }
+      } else {
+        const guestCart = JSON.parse(localStorage.getItem('guestCart')) || [];
+        const count = guestCart.reduce((total, item) => total + item.quantite, 0);
+        setPanierCount(count);
+      }
+    };
+
+    fetchPanierCount();
+  }, []);
+
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
@@ -32,8 +61,14 @@ function Navbar() {
           <VscSearch />
         </button>
 
+        {/* <button className="cart-button" onClick={handleCartClick}>
+          <ShoppingCart />
+        </button> */}
         <button className="cart-button" onClick={handleCartClick}>
           <ShoppingCart />
+          {panierCount > 0 && (
+            <span className="cart-dot"></span> // Afficher un point rouge si panierCount > 0
+          )}
         </button>
         <button className="menu-button" onClick={toggleMenu}>
           <VscThreeBars />
